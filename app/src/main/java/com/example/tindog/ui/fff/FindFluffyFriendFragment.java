0ceb1841c8cb8172c5016ab3767b2081a616f3f6
Guy <guy.kasher@mail.huji.ac.il>
2021-09-04 18:52:18 +0300

    add dogs to tindog

--- app/src/main/java/com/example/tindog/ui/fff/FindFluffyFriendFragment.java
+++ app/src/main/java/com/example/tindog/ui/fff/FindFluffyFriendFragment.java
@@ -1,5 +1,6 @@
 package com.example.tindog.ui.fff;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tindog.R;
+import com.example.tindog.ui.tinder.AddNewDog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -51,6 +54,15 @@ public class FindFluffyFriendFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_fluffy_friend, container, false);
+        View view=inflater.inflate(R.layout.fragment_find_fluffy_friend, container, false);
+        FloatingActionButton add_button = (FloatingActionButton) view.findViewById(R.id.add_Friend);
+        FloatingActionButton remove_button = (FloatingActionButton) view.findViewById(R.id.removeFriend);
+
+        add_button.setOnClickListener(v -> {
+            Intent intentToOpenAddDog = new Intent(v.getContext(), AddNewDog.class);
+            v.getContext().startActivity(intentToOpenAddDog);
+
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/tindog/ui/tinder/openFragment.java
+++ app/src/main/java/com/example/tindog/ui/tinder/openFragment.java
@@ -1,4 +1,16 @@
 package com.example.tindog.ui.tinder;
 
-public class openFragment {
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class openFragment extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new DogFragment()).commit();}
+    }
 }
+
