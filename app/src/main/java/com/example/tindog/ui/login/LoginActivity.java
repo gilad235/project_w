42361eaf8ec196583904402e1a1ed9da184be0d2
Guy <guy.kasher@mail.huji.ac.il>
2021-09-14 01:53:12 +0300

    commit

--- app/src/main/java/com/example/tindog/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/tindog/ui/login/LoginActivity.java
@@ -48,7 +48,8 @@ public class LoginActivity extends AppCompatActivity {
 
 //        binding = ActivityLoginBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
-        final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+        final ActivityResultLauncher<Intent> signInLauncher =
+                registerForActivityResult(
                 new FirebaseAuthUIActivityResultContract(),
                 new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
                     @Override
